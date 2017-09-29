ef4a247dbdbd43c123f9317a39ef1834038d141f
Mihai Mogosanu <mike@sapiensworks.com>
2017-09-29 13:58:28 -0400

    refactored create storage for idempotency store

--- src/CavemanTools.Persistence.Sql/IdempotencyTools.cs
+++ src/CavemanTools.Persistence.Sql/IdempotencyTools.cs
@@ -20,49 +20,25 @@ namespace CavemanTools.Persistence.Sql
             public DateTime UtcTimestamp { get; set; }  =DateTime.UtcNow;
         }
 
+        static void ConfigSql(SqlFuConfig cfg, TableName name) =>
+            cfg.ConfigureTableForPoco<IdemStore>(t => t.TableName = name);
+
         public static void CreateSqliteStorage(DbConnection db)
-            => InitStorage(db, CreateSqlite());
-        public static void CreateMSSqlStorage(DbConnection db,string schema)
-            => InitStorage(db, CreateMSSql(new TableName(DefaultTableName,schema??DefaultSchema)));
-
-
-        private static void InitStorage(DbConnection db,string sql,string schema = null)
+            => db.CreateStorage(p =>
+                    $"create table if not exists {DefaultTableName} (Hash text primary key not null, UtcTimestamp text not null)",
+                c => ConfigSql(c, DefaultTableName)
+            );            
+        public static void CreateMSSqlStorage(DbConnection db,string schema=null)
         {
-            db.AddDbObjectOrIgnore(sql);            
+            var name = new TableName(DefaultTableName, schema ?? DefaultSchema);
+            db.CreateStorage(prov=>
+            $"create table  {prov.EscapeTableName(name)} (Hash char(32) primary key not null, UtcTimestamp date not null)"
+            ,c=>ConfigSql(c,name));
+                        
         }
 
-        ///// <summary>
-        ///// 
-        ///// </summary>
-        ///// <typeparam name="T"></typeparam>
-        ///// <param name="factory"></param>
-        ///// <param name="name"></param>
-        ///// <param name="schema"></param>
-        ///// <param name="ifExists"></param>
-        //public static void InitStorage<T>(T factory,string name=DefaultTableName,string schema=DefaultSchema,TableExistsAction ifExists=TableExistsAction.Ignore) where T : IDbFactory        
-        //{
-        //    new StoreCreator(factory).WithTableName(name, schema).IfExists(ifExists).Create();
-        //    //SqlFuManager.Config.ConfigureTableForPoco<IdemStore>(
-        //    //    c => c.Table = new SqlFu.Configuration.TableName(name, schema));
-        //}
 
-        //public class StoreCreator : ATypedStorageCreator<IdemStore>
-        //{
-
-         
-        //    protected override void Configure(IConfigureTable<IdemStore> cfg)
-        //    {
-        //        cfg
-        //        .Column(ta => ta.Hash, c => c.HasDbType("char").HasSize(32))
-        //        .PrimaryKey(pk => pk.OnColumns(d => d.Hash))
-        //        .HandleExisting(HandleExistingTable);
-        //    }
-
-        //    public StoreCreator(IDbFactory db) : base(db)
-        //    {
-        //    }
-        //}
-        public static bool IsDuplicateOperation(this DbConnection db, IdempotencyId data)
+     public static bool IsDuplicateOperation(this DbConnection db, IdempotencyId data)
         {
             data.MustNotBeNull();
             try
@@ -91,11 +67,7 @@ namespace CavemanTools.Persistence.Sql
             return false;
         }
 
-        static string CreateSqlite() => $"create table if not exists {DefaultTableName} (Hash text primary key not null, UtcTimestamp text not null)";
-        
-
-        static string CreateMSSql(TableName name)
-        => $"create table  {name} (Hash char(32) primary key not null, UtcTimestamp date not null)";
+       
     }
     
   
--- src/Tests/SqliteIdempotentcyTests.cs
+++ src/Tests/SqliteIdempotentcyTests.cs
@@ -1,9 +1,11 @@
 ﻿using System.Data.Common;
+using System.Data.SqlClient;
 using System.Data.SQLite;
 using System.IO;
 using CavemanTools.Persistence.Sql;
 using SqlFu;
 using SqlFu.Providers.Sqlite;
+using SqlFu.Providers.SqlServer;
 using Xunit;
 
 namespace Tests
@@ -28,6 +30,34 @@ namespace Tests
         protected override void Init()
         {
             IdempotencyTools.CreateSqliteStorage(_db);
+        }
+
+        protected override void DisposeOther()
+        {
+            _db.Execute($"drop table {_db.GetTableName<IdempotencyTools.IdemStore>()}");
+        }
+    }
+
+    [Collection("SqlServer")]
+    public class SqlServerIdemTest : AIdempotencyTests
+    {
+        private IDbFactory _factory;
+
+        public SqlServerIdemTest()
+        {
+
+        }
+
+        protected override DbConnection GetConnection()
+        {
+            _factory = Setup.DbFactory(new SqlServer2012Provider(SqlClientFactory.Instance.CreateConnection),
+                Setup.SqlServerConnectionString);
+            return _factory.Create();
+        }
+
+        protected override void Init()
+        {
+            IdempotencyTools.CreateMSSqlStorage(_db);
         }
 
         protected override void DisposeOther()
